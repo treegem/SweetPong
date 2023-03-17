7b6d729d8d54bfadd6ac635ba21271909eaae35e
gbrown <georg.braunbeck@googlemail.com>
2023-03-17 22:35:57 +0100

    clean up

--- scripts/CountdownLabel.gd
+++ scripts/CountdownLabel.gd
@@ -1,10 +1,10 @@
 extends Label
 
 func _process(delta):
-	text = str(int($CoutndownTimer.time_left) + 1)
+	text = str(int($CountdownTimer.time_left) + 1)
 
 func count_down():
-	$CoutndownTimer.start()
+	$CountdownTimer.start()
 	show()
 
 func _on_coutndown_timer_timeout():
--- scripts/EnemyScore.gd
+++ scripts/EnemyScore.gd
@@ -2,5 +2,4 @@ extends Label
 
 
 func _on_main_enemy_scored(score):
-	print("enemy scored")
 	text = str(score)
